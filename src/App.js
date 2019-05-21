4c0f5d98a2a36c5fd27edc4f16846190b130fa69
laralcq <laraloicq@gmail.com>
2019-05-21 09:59:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Lara Loicq</h1>
+      </header>
+      <div className="description">
+        <h2>Front-end developer junior</h2>
+        <h4>Hi ! I'm Lara and I don't care about you.</h4>
+      </div>
+    </div>
+  );
+}
+
+export default App;
